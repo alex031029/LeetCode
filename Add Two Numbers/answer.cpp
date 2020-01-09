0fe1b1b0082f255d5438b79b21384c8488accd4b
Yanchen SHI <alex920101094@163.com>
2020-01-09 23:58:57 +0800

    fix a bug of case [1] and [9,9]

--- Add Two Numbers/answer.cpp
+++ Add Two Numbers/answer.cpp
@@ -11,12 +11,14 @@ public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode * p1 = l1;
         ListNode * p2 = l2;
+        // initialize a root with 0 value;
         ListNode * ret = new ListNode(0);
         ListNode * root = ret;
         int carry = 0;
         for(;p1!=NULL||p2!=NULL;)
         {
             int temp = 0;
+            // add up from the smallest digit
             if(p1!=NULL)
             {
                 temp+= p1->val;
@@ -28,15 +30,18 @@ public:
                 temp+=p2->val;
                 p2=p2->next;
             }
-            ListNode * tempNode = new ListNode(temp%10+carry);
-            carry = temp/10;
+            // the carry is taken into consideration
+            ListNode * tempNode = new ListNode((temp+carry)%10);
+            carry = (temp+carry)/10;
             ret->next = tempNode;
             ret = tempNode;
         }
+        // add the highest digit of 1 if the carry is 1
         if(carry!=0)
         {
             ret->next = new ListNode(carry);
         }
+        // the first node of root is useless, return the next node
         return root->next;
     }
 };
