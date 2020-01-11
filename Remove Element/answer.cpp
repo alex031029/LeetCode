49362db9459c49a9e9c9246dfb9a8f1e64c3783a
Yanchen SHI <alex920101094@163.com>
2020-01-11 22:49:05 +0800

    complete this question and add comments

--- Remove Element/answer.cpp
+++ Remove Element/answer.cpp
@@ -1,25 +1,46 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
+        // special case of nums.size()==0 or 1
+        if(nums.empty())
+            return 0;
+        if(nums.size()==1)
+        {
+            return nums[0]==val?0:1;
+        }
+        // two pointers, traversing forward and backward respecitvely
         int f=0;
         int b=nums.size()-1;
+        
+        // a lambda function to move backward pointer
         auto moveBackPointer = [&]()->bool{
-            while(b>=f&&nums[b]==val)
+            while(b>f&&nums[b]==val)
             {
                 b--;
             }
-            return (b>=f);
+            return (b>f);
         };
         while(b>f)
         {
-            if(nums[b]==val)
+            // swap nums[f] if it is equal to val
+            if(nums[f]==val)
             {
                 if(!moveBackPointer())
+                {
                     break;
+                }
                 nums[f]=nums[b];
+                // cout<<f<<" "<<b<<endl;
+                b--;
+                // the continue statement assures each iteration move only one step (either b or f)
+                continue;
             }
             f++;
         }
-        return f;
+        // cout<<f<<" "<<b<<endl;
+        
+        // if nums[f]!= val, return f+1
+        // otherwise returns f
+        return f+(nums[f]!=val);
     }
 };
