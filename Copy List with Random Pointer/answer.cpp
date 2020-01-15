6d14707d299aa03b656ae07c8d35d973dc96bf9e
Yanchen SHI <alex920101094@163.com>
2020-01-16 00:31:20 +0800

    add comments

--- Copy List with Random Pointer/answer.cpp
+++ Copy List with Random Pointer/answer.cpp
@@ -21,12 +21,15 @@ public:
 
         Node * ret = new Node(0);
         Node * p0 = ret;
+        
+        // a hash to determine if a Node has been created.
         map<Node*, Node*> hash;
 
-        int i=0;
         for(Node * p = head; p!=NULL; p=p->next)
         {
-            cout<< p->val<<endl;
+            // cout<< p->val<<endl;
+            
+            // create a Node if it has yet been created 
             Node * temp = NULL;
             if(hash.find(p)==hash.end())
             {
@@ -38,11 +41,16 @@ public:
                 hash[p]->val = p->val;
                 temp = hash[p];
             }
+            
+            // check the random pointer
             Node * r = p->random;
             if(r != NULL)
             {
+                // if the random pointer is referring to a old Node 
+                //  refer to it
                 if(hash.find(r)!=hash.end())
                     temp->random = hash[r];
+                // otherwise, create a new one and refer to it
                 else
                 {
                     Node * temp2 = new Node(r->val);
