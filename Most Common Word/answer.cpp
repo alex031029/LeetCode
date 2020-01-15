fe7f5951eb9411a8c41fa1b9d062b2baf0e67662
Yanchen SHI <alex920101094@163.com>
2020-01-15 23:54:41 +0800

    Update answer.cpp

--- Most Common Word/answer.cpp
+++ Most Common Word/answer.cpp
@@ -1,28 +1,39 @@
 class Solution {
 public:
     string mostCommonWord(string paragraph, vector<string>& banned) {
+        // a hash to count the frequency in the paragraph
         map<string,int> hitRate;
+        // a hash to check if a word is banned
         map<string,bool> hitBanned;
-
+    
+        // initialize hitBanned
         for(int i=0;i<banned.size();i++)
         {
             hitBanned[banned[i]]=true;
         }
+        
         string ret;
         int maxRate = 0;
         for(int i=0;i<paragraph.size();i++)
         {
-            char c = paragraph[i];
             string s = "";
-            if(c>='A'&c<='Z')
-                c = c-'A'+'a';
+            // lowercase a letter
+            auto lowercase = [](char c)->char{
+                if(c>='A'&&c<='Z')
+                    return c-'A'+'a';
+                else
+                    return c;
+            };
+            char c = lowercase(paragraph[i]);
             while(c>='a'&c<='z')
             {
                 s+=c;
                 i++;
-                c = paragraph[i];
+                c = lowercase(paragraph[i]);
             }
-            if(hitBanned[s])
+            
+            // for empty string or banned string, omit it and conitnue 
+            if(hitBanned[s]||s==)
                 continue;
             if(hitRate.find(s)==hitRate.end())
                 hitRate[s] = 1;
