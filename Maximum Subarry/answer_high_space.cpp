6d55df87416150a9662cd2d76423843254f73082
Yanchen SHI <alex920101094@163.com>
2020-01-20 23:58:09 +0800

    add an alternate solution with smaller space

--- Maximum Subarry/answer_high_space.cpp
+++ Maximum Subarry/answer_high_space.cpp
@@ -17,5 +17,26 @@ public:
                 ret = dp[i];
         }
         return ret;
+    }    
+    
+    // an alternate solution with smaller space overheads
+    int maxSubArray2(vector<int>& nums) {
+        if(nums.empty())
+            return 0;
+        
+        // note that in the solution above, dp[] is not necessary
+        // since it is only traversed once, and dp[i] is calculated based only on dp[i-1]
+        // thus, we can use an int dp instead of an array
+        int dp;
+        
+        dp = nums[0];
+        int ret = nums[0];
+        for(int i=1;i<nums.size();i++)
+        {
+            dp = max(dp+nums[i], nums[i]);
+            if(dp>ret)
+                ret = dp;
+        }
+        return ret;
     }
 };
