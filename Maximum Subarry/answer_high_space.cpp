851ccee63cd78b65128961bfeedb0945220a0a00
Yanchen SHI <alex920101094@163.com>
2020-01-20 23:50:09 +0800

    Create answer_high_space.cpp

---
+++ Maximum Subarry/answer_high_space.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        if(nums.empty())
+            return 0;
+
+        int len = nums.size();
+        int dp[len];
+        
+        dp[0] = nums[0];
+        int ret = nums[0];
+        for(int i=1;i<len;i++)
+        {
+            // dp[i] represents the maxmium value of an subarry that ends in nums[i]
+            dp[i] = max(dp[i-1]+nums[i], nums[i]);
+            if(dp[i]>ret)
+                ret = dp[i];
+        }
+        return ret;
+    }
+};
