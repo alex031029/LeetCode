98888f350df8100bfa59bb8908618587844441b9
Yanchen SHI <alex920101094@163.com>
2020-01-22 20:35:46 +0800

    Create answer.cpp

---
+++ Merge Sorted Array/answer.cpp
@@ -0,0 +1,27 @@
+// this solution has an O(n+m) time complexity
+// and O(1) space complexity
+// the basic idea is to use two pointers, traversing both arrays from the last element
+// and use the merge sort algorithm to merge them.
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        // nums1.insert(nums1.end(), nums2.begin(), nums2.end());
+        int p = m+n-1;
+        int i = m-1, j = n-1;
+
+        for(;p>=0;p--)
+        {
+            if(i>=0&&(j<0||nums1[i]>nums2[j]))
+            {
+                nums1[p] = nums1[i];
+                i--;
+            }
+            else
+            {
+                nums1[p] = nums2[j];
+                j--;
+            }
+        }
+    }
+};
