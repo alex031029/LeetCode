674a53cb98142f0b3ccbf0d8975c700096cc95c3
Yanchen SHI <alex920101094@163.com>
2020-02-07 01:48:55 +0800

    Create answer_wrong.cpp

---
+++ Median of Two Sorted Arrays/answer_wrong.cpp
@@ -0,0 +1,49 @@
+// an O(o log n) soluion, but it is wrong on edge cases
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int k = (nums1.size()+nums2.size()+1)/2;
+        int p1=0, p2=0;
+        while(1)
+        {
+            cout<<k<<p1<<p2<<endl;
+            int i = min(int(nums1.size()-1), p1+(k-1)/2);
+            int j = min(int(nums2.size()-1), p2+(k-1)/2);
+            if(nums1[i]<nums2[j])
+            {
+                p1 = i;
+                k = k - p1;
+            }
+            else
+            {
+                p2 = j;
+                k = k - p2;
+            }
+            if(k<=1||p1==nums1.size()-1||p2==nums2.size()-1)
+                break;
+        }
+        cout<<k<<p1<<p2<<endl;
+        if(k>1)
+        {
+            if((nums1.size()+nums2.size())%2==1)
+            {
+                if(p1 == nums1.size()-1)
+                    return nums2[p2+k-1];
+                else
+                    return nums1[p1+k-1];
+            }
+            else
+            {
+                if(p1 == nums1.size()-1)
+                    return double(nums2[p2+k-1]+nums2[p2+k])/2;
+                else
+                    return double(nums1[p1+k-1]+nums1[p1+k])/2;
+            }
+        }
+        if((nums1.size()+nums2.size())%2==1)
+            return min(nums1[p1],nums2[p2]);
+        else
+            return double(nums1[p1]+nums2[p2])/2;
+    }
+};
