87539c9eff23b6b886caf683cd910f14906ebfd4
Yanchen SHI <alex920101094@163.com>
2020-02-08 01:31:35 +0800

    make it more succinct

--- Product of Array Except Self/answer.cpp
+++ Product of Array Except Self/answer.cpp
@@ -2,26 +2,24 @@
 // the basic idea is that 
 // 乘积 = 左边的乘积*右边的乘积！
 
+
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         vector<int> ret = nums;
-
+        
         int prev = 1;
         // calculate the left product
-        for(int i=0;i<ret.size()-1;i++)
+        for(int i=0;i<ret.size();i++)
         {
             ret[i] = prev;
             prev*=nums[i];
         }
         // now ret[i] is the product of ret[0,..,i-1]
         
-        // set ret[n-1]
-        ret[ret.size()-1] = prev;
-        
+        prev = 1;
         // calculate the right product times left product
-        prev = nums[nums.size()-1];
-        for(int i=ret.size()-2;i>=0;i--)
+        for(int i=ret.size()-1;i>=0;i--)
         {
             ret[i]*=prev;
             prev*=nums[i];
