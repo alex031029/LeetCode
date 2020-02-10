9595647faa22a1aedca59ab9651e0a5c37aca7e2
Yanchen SHI <alex920101094@163.com>
2020-02-11 00:42:29 +0800

    remove obsolete debuggers

--- Permutations/answer.cpp
+++ Permutations/answer.cpp
@@ -2,20 +2,20 @@
 
 class Solution {
 public:
-    vector<vector<int>> helper(vector<int>& nums, vector<bool>& flag) 
+    vector<vector<int>> helper(vector<int>& nums, vector<bool>& flag, int cnt) 
     {
         vector<vector<int>> ret;
-        // cout<<flag[0]<<endl;
+        // return an empty vector if all elements in flag is true
+        if(cnt == nums.size())
+            return ret;
+
         for(int i=0;i<nums.size();i++)
         {
-            // cout<<i<<endl;
             if(flag[i])
                 continue;   
             flag[i] = true;
-            auto result = helper(nums, flag);
-            // cout<<i<<endl;
-            
-            // Do not forget the trivial case
+            auto result = helper(nums, flag, cnt+1);
+
             if(result.empty())
                 ret.push_back(vector<int>{nums[i]});
             for(int j=0;j<result.size();j++)
@@ -24,18 +24,19 @@ public:
                 temp.push_back(nums[i]); 
                 temp.insert(temp.end(), result[j].begin(), result[j].end());
                 ret.push_back(temp);
-                // cout<<temp[0]<<endl;
+
             }
             flag[i] = false;
-            // ret.push_back(temp);      
+  
         }
         return ret;
     }
-    
     vector<vector<int>> permute(vector<int>& nums) {
-        // flag records the used elements
+        // flag is to record used elements
         vector<bool> flag(nums.size(),false);
-        // cout<<"haha"<<flag[0]<<endl;
-        return helper(nums, flag);
+        // cnt is the number of true elements in flag
+        int cnt = 0;
+
+        return helper(nums, flag, cnt);
     }
 };
