a7d8e125ee856746e14c2ee8932346c9def818f2
Yanchen SHI <alex920101094@163.com>
2020-02-12 00:14:09 +0800

    Create answer.cpp

---
+++ Missing Ranges/answer.cpp
@@ -0,0 +1,66 @@
+// some tricky cases including INT_MAX and INT_MIN values
+
+class Solution {
+public:
+    string makeInterval(long int a, long int b)
+    {
+        if(a>b)
+            return "";
+        else if(a==b)
+            return to_string(a);
+        else
+            return to_string(a)+"->"+to_string(b);
+    }
+
+    vector<string> findMissingRanges(vector<int>& nums, int lower, int upper) {
+        vector<string> ret;
+        if(nums.empty())
+        {
+            if(lower>upper)
+                return ret;
+            else if(lower == upper)
+            {
+                ret.push_back(to_string(lower));
+                return ret;
+            }
+            else
+            {
+                ret.push_back(to_string(lower)+"->"+to_string(upper));
+                return ret;
+            }
+        }
+        
+        // long type to prevent overflow of input
+        int i=0;
+        long int prev = long(lower)-1;
+        for(;i<nums.size()&&nums[i]<=upper;prev=nums[i++])
+        {
+            if(nums[i]<lower)
+                continue;
+            if(nums[i]-prev<=1)
+                continue;
+            if(nums[i]-prev==2)
+            {
+                ret.push_back(to_string(nums[i]-1));
+            }
+            else
+            {
+                ret.push_back(to_string(max(long(lower),prev+1))+"->"+to_string(nums[i]-1));
+            }
+        }
+        // cout<<ret.size();
+        if(i==nums.size())
+        {
+            ret.push_back(makeInterval(nums.back()+long(1), upper));
+            if(ret.back().empty())
+                ret.pop_back();
+        }
+        else
+        {
+            ret.push_back(makeInterval(nums[i-1]+1, upper));
+            if(ret.back().empty())
+                ret.pop_back();
+        }
+        return ret;
+    }
+};
