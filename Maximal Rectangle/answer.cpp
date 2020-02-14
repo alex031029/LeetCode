5a854acc1d24949e1829d2656340918fab124354
Yanchen SHI <alex920101094@163.com>
2020-02-14 19:09:59 +0800

    Create answer.cpp

---
+++ Maximal Rectangle/answer.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) { 
+        // add a 0 at the beginning and ending respectively of heights 
+        // heights.insert(heights.begin(),0);
+        // heights.push_back(0);
+
+        int ret = 0;
+
+        stack<int> stk;
+        for(int i=0;i<heights.size();i++)
+        {
+            if(stk.empty()||heights[stk.top()]<=heights[i])
+                stk.push(i);
+            else
+            {
+                while(heights[stk.top()]>heights[i])
+                {
+                    int prev = stk.top();
+                    stk.pop();
+                    int temp = (i-stk.top()-1)*heights[prev];
+                    if(ret<temp)
+                        ret = temp;
+                }
+                stk.push(i);
+            }
+        }      
+        return ret;        
+    }
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        if(matrix.empty())
+            return 0;
+        vector<int> heights(matrix[0].size()+2, 0);
+        int ret = 0;
+        for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=0;j<matrix[0].size();j++)
+            {
+                heights[j+1] = matrix[i][j]=='0'?0:heights[j+1]+1;
+            }
+            int temp = largestRectangleArea(heights);
+            if(temp>ret)
+                ret = temp;
+        }
+        return ret;
+    }
+};
