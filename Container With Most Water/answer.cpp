f20907ddf48d8ac5d210db98fb3cbfa81961bc7f
Yanchen SHI <alex920101094@163.com>
2020-02-20 23:39:53 +0800

    Create answer.cpp

---
+++ Container With Most Water/answer.cpp
@@ -0,0 +1,27 @@
+
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        if(height.size()<2)
+            return 0;
+
+        int left = 0;
+        int right = height.size()-1;
+
+        int ret = 0;
+        while(right>left)
+        {
+            int temp = min(height[left],height[right])*(right-left);
+            if(temp>ret)
+                ret = temp;
+            if(height[left]>height[right])
+            {
+                right--;
+            }
+            else   
+                left++;
+        }
+        return ret;
+    }
+};
