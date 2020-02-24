19b4d632be4d189aaf16807fc5fb59d2e3fe697f
Yanchen SHI <alex920101094@163.com>
2020-02-25 02:59:35 +0800

    add comments

--- Divide Two Integers/Answer.c
+++ Divide Two Integers/Answer.c
@@ -1,3 +1,5 @@
+// Basically we use left shift to implemenet a division
+
 int divide(int dividend, int divisor) {
     bool negative_flag=0;
     unsigned long long int dividend_2,divisor_2;
@@ -23,7 +25,7 @@ int divide(int dividend, int divisor) {
         divisor_2=divisor;
         dividend_2=dividend;
     }
-    printf("%ld,%ld,%d\n",dividend_2,divisor_2,negative_flag);
+    // printf("%ld,%ld,%d\n",dividend_2,divisor_2,negative_flag);
     unsigned long long int ret=0,i=0;
     while(dividend_2>=divisor_2)
     {
@@ -33,14 +35,14 @@ int divide(int dividend, int divisor) {
             temp=temp<<1;
         }
         //printf("haha%d\n",i);
-        ret+=1<<(i-1);
-        printf("%ld,%ld\n",i,ret);
+        // (long long) is to prevent an overflow
+        ret+=(long long)1<<(i-1);
+        // printf("%ld,%ld\n",i,ret);
         dividend_2-=temp>>1;
     }
-    printf("%ld\n",ret);
+    // printf("%ld\n",ret);
     if((ret>2147483647 && negative_flag==0)||ret<0)
         return 2147483647;
     return (negative_flag==1)?-ret:ret;
 }
-
 
