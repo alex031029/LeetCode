5d58110dc7f6146fa47681b37a5044bede164ab7
Yanchen SHI <alex920101094@163.com>
2020-02-26 00:42:49 +0800

    Create answer_high_space.cpp

---
+++ Find the Duplicate Number/answer_high_space.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int hash[nums.size()];
+        memset(hash,0,sizeof(int)*nums.size());
+
+        for(int i=0;i<nums.size();i++)
+        {
+            hash[nums[i]]++;
+            if(hash[nums[i]]>=2)
+                return nums[i];
+        }
+        return 0;
+    }
+};
