2a29601c008bc240f3808f4977345f52aa6dc821
Yanchen SHI <alex920101094@163.com>
2020-03-02 23:26:12 +0800

    a slow version

---
+++ Implement Queue using Stacks/answer_slow.cpp
@@ -0,0 +1,62 @@
+// O(n) for pop and top operations, O(1) for push
+
+class MyQueue {
+public:
+    stack<int> s1;
+    stack<int> s2;
+    /** Initialize your data structure here. */
+    MyQueue() {
+    }
+    
+    /** Push element x to the back of queue. */
+    void push(int x) {
+        s1.push(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        while(!s1.empty())
+        {
+            s2.push(s1.top());
+            s1.pop();
+        }
+        int ret = s2.top();
+        s2.pop();
+        while(!s2.empty())
+        {
+            s1.push(s2.top());
+            s2.pop();
+        }
+        return ret;
+    }
+    
+    /** Get the front element. */
+    int peek() {
+        while(!s1.empty())
+        {
+            s2.push(s1.top());
+            s1.pop();
+        }
+        int ret = s2.top();
+        while(!s2.empty())
+        {
+            s1.push(s2.top());
+            s2.pop();
+        }
+        return ret;
+    }
+    
+    /** Returns whether the queue is empty. */
+    bool empty() {
+        return s1.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
