14f2455f3178b0f436b87de27ddd815f8521a032
Yanchen SHI <alex920101094@163.com>
2020-03-13 23:16:48 +0800

    log n solution

---
+++ Pow(x, n)/answer.cpp
@@ -0,0 +1,26 @@
+// a binary recusive solution 
+// O(log n) time complexity
+
+class Solution {
+public:
+    double fastPow(double x, long long n) {
+        if (n == 0) {
+            return 1.0;
+        }
+        double half = fastPow(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+    
+    double myPow(double x, int n) {
+        long long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        return fastPow(x, N);
+    }
+};
