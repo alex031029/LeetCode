227d9a559b6d7aa6f51dd4ca5ce643e7262febc8
Yanchen SHI <alex920101094@163.com>
2020-03-14 14:15:59 +0800

    add question description

--- interviews/Microsoft/SecondAttempt/Q2.cpp
+++ interviews/Microsoft/SecondAttempt/Q2.cpp
@@ -1,23 +1,23 @@
-We write the integers of A and B (in the order they are given) on two separate horizontal lines.
+// We write the integers of A and B (in the order they are given) on two separate horizontal lines.
 
-Now, we may draw a straight line connecting two numbers A[i] and B[j] as long as A[i] == B[j], and the line we draw does not intersect any other connecting (non-horizontal) line.
+// Now, we may draw a straight line connecting two numbers A[i] and B[j] as long as A[i] == B[j], and the line we draw does not intersect any other connecting (non-horizontal) line.
 
-Return the maximum number of connecting lines we can draw in this way.
+// Return the maximum number of connecting lines we can draw in this way.
 
-Example 1:
+// Example 1:
 
-Input: A = [1,4,2], B = [1,2,4]
-Output: 2
-Explanation: We can draw 2 uncrossed lines as in the diagram.
-We cannot draw 3 uncrossed lines, because the line from A[1]=4 to B[2]=4 will intersect the line from A[2]=2 to B[1]=2.
-Example 2:
+// Input: A = [1,4,2], B = [1,2,4]
+// Output: 2
+// Explanation: We can draw 2 uncrossed lines as in the diagram.
+// We cannot draw 3 uncrossed lines, because the line from A[1]=4 to B[2]=4 will intersect the line from A[2]=2 to B[1]=2.
+// Example 2:
 
-Input: A = [2,5,1,2,5], B = [10,5,2,1,5,2]
-Output: 3
-Example 3:
+// Input: A = [2,5,1,2,5], B = [10,5,2,1,5,2]
+// Output: 3
+// Example 3:
 
-Input: A = [1,3,7,1,7,5], B = [1,9,2,5,1]
-Output: 2
+// Input: A = [1,3,7,1,7,5], B = [1,9,2,5,1]
+// Output: 2
 
 
 int maximumLine(vector<int>& A, vector<int>& B)
