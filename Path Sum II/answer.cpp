a0c92e1adf55449269dd3287fb9f229065093b63
Yanchen SHI <alex920101094@163.com>
2020-03-23 01:37:55 +0800

    a dfs solution

--- Path Sum II/answer.cpp
+++ Path Sum II/answer.cpp
@@ -1,5 +1,5 @@
 // a DFS solution
-// but it turned out to be quite slow
+// I improved the dfs function to make it more efficient
 
 /**
  * Definition for a binary tree node.
@@ -10,13 +10,13 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-
-
 class Solution {
 public:
     int sum;
     vector<vector<int>> ret;
-    void dfs(TreeNode * root, int offset, vector<int> path)
+    
+    // use the reference of path to improve time efficiency
+    void dfs(TreeNode * root, int offset, vector<int>& path)
     {
         if(root==NULL)
             return;
@@ -25,17 +25,25 @@ public:
         if(offset==sum&&root->left==NULL&&root->right==NULL)
         {
             ret.push_back(path);
+            // do not forget to pop_back
+            path.pop_back();
             return;
         }
         dfs(root->left, offset, path);
         dfs(root->right, offset, path);
+        
+        // do not forget to pop_back
+        path.pop_back();
     }
 
     vector<vector<int>> pathSum(TreeNode* root, int sum) {
         if(root==NULL)
             return {};
         this->sum = sum;
-        dfs(root, 0, vector<int>{});
+        
+        // use the reference can improve the time efficiency by a large margin
+        vector<int> path;
+        dfs(root, 0, path);
         return ret;
     }
 };
