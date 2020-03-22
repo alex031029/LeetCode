0aece4fbd80940580a21bc4907b1a5c5620c411c
Yanchen SHI <alex920101094@163.com>
2020-03-22 21:57:42 +0800

    a n log n solution

---
+++ Minimum Increment to Make Array Unique/answer_slow.cpp
@@ -0,0 +1,29 @@
+// an O(n log n) solution
+// using sort
+
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& A) {
+        if(A.size()<=1)
+            return 0;
+        sort(A.begin(), A.end());
+        // for(int i=0;i<A.size();i++)
+        //     cout<<A[i]<<"\t";
+        // cout<<endl;
+        int ret = 0;
+
+
+        for(int i=1;i<A.size();i++)
+        {
+            if(A[i]<=A[i-1])
+            {
+                ret += A[i-1]+1-A[i];
+                A[i] = A[i-1]+1;
+            }
+        }
+        // for(int i=0;i<A.size();i++)
+        //     cout<<A[i]<<"\t";
+        // cout<<endl;
+        return ret;
+    }
+};
