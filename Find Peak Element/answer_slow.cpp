2b8be2e632f06580a717c685b257f2f11b6e9834
Yanchen SHI <alex920101094@163.com>
2020-04-03 00:14:11 +0800

    a linear solution

---
+++ Find Peak Element/answer_slow.cpp
@@ -0,0 +1,20 @@
+// a linear solution
+// a trivial implementation
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        if(nums.size()==1) 
+            return 0;
+        if(nums[0]>nums[1])
+            return 0;
+        if(nums[nums.size()-2]<nums.back())
+            return nums.size()-1;
+        for(int i=1;i<nums.size()-1;i++)
+        {
+            if(nums[i]>nums[i-1]&&nums[i]>nums[i+1])
+                return i;
+        }
+        return 0;
+    }
+};
