decebacc1cb589421338b99463b772906c85c27a
Yanchen SHI <alex920101094@163.com>
2020-04-04 20:31:48 +0800

    simplify wordy statements

--- Two Sum/answer_slow.java
+++ Two Sum/answer_slow.java
@@ -1,19 +1,17 @@
 // the very first code in Java for LeetCode!
 // slow implementation, but straightforward
 
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int ret[] = new int[2];
         for(int i=0;i<nums.length;i++)
         {
             for(int j=i+1;j<nums.length;j++)
                 if(nums[i]+nums[j]==target)
                 {
-                    ret[0] = i;
-                    ret[1] = j;
-                    return ret;
+                    return new int[] { i, j };
                 }
         }
-        return ret;
+        return new int[] {0, 0};
     }
 }
