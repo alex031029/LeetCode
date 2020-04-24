5593f34b0a6b305924b07ff28784bbc4df066525
Yanchen SHI <alex920101094@163.com>
2020-04-24 15:22:41 +0800

    Create answer.cpp

---
+++ 面试题51. 数组中的逆序对/answer.cpp
@@ -0,0 +1,50 @@
+// a merge sort method
+// LeetCode offcial solution
+
+class Solution {
+public:
+    // seperata nums into two sub arrays 
+    // recusively call the function to merge it
+    int mergeSort(vector<int>& nums, vector<int>& tmp, int l, int r) {
+        if (l >= r) {
+            return 0;
+        }
+
+        int mid = (l + r) / 2;
+        
+        // number of inverted counter of two sub arrays
+        int inv_count = mergeSort(nums, tmp, l, mid) + mergeSort(nums, tmp, mid + 1, r);
+        int i = l, j = mid + 1, pos = l;
+        while (i <= mid && j <= r) {
+            if (nums[i] <= nums[j]) {
+                tmp[pos] = nums[i];
+                ++i;
+                // for each nums[j] larger than nums[i]
+                // augument inv_count
+                inv_count += (j - (mid + 1));
+            }
+            else {
+                tmp[pos] = nums[j];
+                ++j;
+            }
+            ++pos;
+        }
+        for (int k = i; k <= mid; ++k) {
+            tmp[pos++] = nums[k];
+            inv_count += (j - (mid + 1));
+        }
+        for (int k = j; k <= r; ++k) {
+            tmp[pos++] = nums[k];
+        }
+        
+        // copy temp to nums
+        copy(tmp.begin() + l, tmp.begin() + r + 1, nums.begin() + l);
+        return inv_count;
+    }
+
+    int reversePairs(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> tmp(n);
+        return mergeSort(nums, tmp, 0, n - 1);
+    }
+};
