02aa987ba90aa3734a9493308490762d70d026b8
Yanchen SHI <alex920101094@163.com>
2020-05-11 09:24:39 +0800

    Create answer.cs

---
+++ Pow(x, n)/answer.cs
@@ -0,0 +1,23 @@
+// a C# implementation
+
+public class Solution {
+    public double fastPow(double x, long n) {
+        if (n == 0) {
+            return 1.0;
+        }
+        double half = fastPow(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+    public double MyPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        return fastPow(x, N);
+    }
+}
