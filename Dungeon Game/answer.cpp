c55ab6472b451cb33d3679e65d6518f1ee14c6a0
Yanchen SHI <alex920101094@163.com>
2020-07-12 23:43:01 +0800

    add comment

--- Dungeon Game/answer.cpp
+++ Dungeon Game/answer.cpp
@@ -6,6 +6,7 @@ public:
     int calculateMinimumHP(vector<vector<int>>& dungeon) {
         int n = dungeon.size(), m = dungeon[0].size();
         vector<vector<int>> dp(n + 1, vector<int>(m + 1, INT_MAX));
+        // dp[i][j] means the smallest HP required for knight to begin from [i][j] to the destination (the right buttom element)
         dp[n][m - 1] = dp[n - 1][m] = 1;
         for (int i = n - 1; i >= 0; --i) {
             for (int j = m - 1; j >= 0; --j) {
