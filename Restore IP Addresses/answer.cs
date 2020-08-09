b8f24c1d464759ebf8e599416f725c15d0bd0638
Yanchen SHI <alex920101094@163.com>
2020-08-09 22:26:33 +0800

    a brute and force c# solution

---
+++ Restore IP Addresses/answer.cs
@@ -0,0 +1,34 @@
+// interpretted from c++ version
+// a brute and force solution
+
+public class Solution {
+    public IList<string> RestoreIpAddresses(string s) {
+        IList<string> ret = new List<string>();
+        for(int a=1;a<4;a++)
+        {
+            for(int b=1;b<4;b++)
+            {
+                for(int c=1;c<4;c++)
+                {
+                    for(int d=1;d<4;d++)
+                    {
+                        if(a+b+c+d!=s.Length)
+                            continue;
+                        int s1 = Convert.ToInt32(s.Substring(0, a));
+                        int s2 = Convert.ToInt32(s.Substring(a, b));
+                        int s3 = Convert.ToInt32(s.Substring(a + b, c));
+                        int s4 = Convert.ToInt32(s.Substring(a+b+c));
+                        if(s1<=255&&s2<=255&&s3<=255&&s4<=255)
+                        {
+                            // it is to remove the meaning less 0 in the integers 
+                            string ip = Convert.ToString(s1)+'.'+Convert.ToString(s2)+'.'+Convert.ToString(s3)+'.'+Convert.ToString(s4);
+                            if(ip.Length == s.Length+3)
+                                ret.Add(ip);
+                        }
+                    }
+                }
+            }
+        }
+        return ret; 
+    }
+}
